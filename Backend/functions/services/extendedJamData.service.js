d1d75f65ccbcf97fd466540c5abf683e5bade7d3
Fabian Friedrich <68951860+Fabianofski@users.noreply.github.com>
2023-02-21 15:28:25 +0100

    Added game_info_panel to jamdata

--- Backend/functions/services/extendedJamData.service.js
+++ Backend/functions/services/extendedJamData.service.js
@@ -1,20 +1,24 @@
 const { fetchGamePage } = require("./gamePage.service");
+const { postJamData } = require("./db.service");
 
 async function fetchExtendedJamData(job) {
   console.log("fetch data for: " + job.data.jam.Title);
 
   const jamData = job.data;
   const gameIds = Object.keys(jamData.jam_games);
-  for (let i = 0; i < 1; i += 1) {
+  for (let i = 0; i < gameIds.length; i += 5) {
+    job.updateProgress((i / gameIds.length).toFixed(2));
     await Promise.all([
-      ...gameIds.slice(i, i + 1).map((id) => {
-        jamData.jam_games[id] = fetchGamePage(jamData.jam_games[id]);
+      ...gameIds.slice(i, i + 5).map(async (id) => {
+        jamData.jam_games[id] =
+          (await fetchGamePage(jamData.jam_games[id])) || jamData.jam_games[id];
       }),
       new Promise((resolve) => {
         setTimeout(resolve, 1000);
       }),
     ]);
   }
+  await postJamData(jamData._id, jamData);
 }
 
 module.exports = { fetchExtendedJamData };
--- Backend/functions/services/gamePage.service.js
+++ Backend/functions/services/gamePage.service.js
@@ -1,6 +1,8 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+const skippedParameters = ["platforms", "author"];
+
 async function fetchGamePage(entry) {
   const response = await axios
     .get(entry.url)
@@ -8,17 +10,15 @@ async function fetchGamePage(entry) {
   if (!response) return;
   const html = response.data;
   const $ = cheerio.load(html);
-
-  $(".game_info_panel_widget table tbody")
+  entry.game_info_panel = {};
+  $(".game_info_panel_widget tbody")
     .children()
     .each(function (idx, tr) {
-      $(tr)
-        .children()
-        .each(function (idx, td) {
-          console.log($(td).text() + "\n");
-        });
+      const title = $(tr).children().first().text().toLowerCase();
+      const content = $(tr).children().first().next().text().toLowerCase();
+      if (!skippedParameters.includes(title))
+        entry.game_info_panel[title] = content.split(", ") || [];
     });
-
   return entry;
 }
 
