38e3fb2c87b81269665e735b6ee02bd7e62dbde2
Fabian Friedrich <friedrich.fabian@gmx.net>
2023-03-25 22:29:12 +0100

    Added Recommended Section

--- jamalyzer-client/app/home/HomeRecommended.tsx
+++ jamalyzer-client/app/home/HomeRecommended.tsx
@@ -1,10 +1,32 @@
 import React from "react";
+import styles from "@/styles/home/HomeRecommended.module.css";
+import jamList from "@/public/jamList.json";
+import { jamCard } from "@/model/jamData/jamCard";
+
+// function shuffle(array: jamCard[]): jamCard[] {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [array[i], array[j]] = [array[j], array[i]];
+//   }
+//   return array;
+// }
+
+const jams = jamList.jams;
 
 function HomeRecommended() {
 
   return (
-    <div>
-      <h1>Recommended</h1>
+    <div className={styles["home-recommended-section"]}>
+      <div className={styles["card-grid"]}>
+        <div className={styles.card}></div>
+        <div className={styles.card}></div>
+        <div className={styles.card}></div>
+        <div className={styles.card}></div>
+        <div className={styles.card}></div>
+        <div className={styles.card}></div>
+        <div className={styles.card}></div>
+        <div className={styles.card}></div>
+      </div>
     </div>
   );
 }
--- jamalyzer-client/app/page.tsx
+++ jamalyzer-client/app/page.tsx
@@ -1,19 +1,9 @@
 import React, { ReactElement } from "react";
-import { jamCard } from "@/model/jamData/jamCard";
-import jamList from "../public/jamList.json";
 import HeroSection from "@/app/home/HeroSection";
 import homeStyles from "@/styles/home/Home.module.css";
 import HomeRecommended from "@/app/home/HomeRecommended";
 
-// function shuffle(array: jamCard[]): jamCard[] {
-//   for (let i = array.length - 1; i > 0; i--) {
-//     const j = Math.floor(Math.random() * (i + 1));
-//     [array[i], array[j]] = [array[j], array[i]];
-//   }
-//   return array;
-// }
 
-const jams = jamList.jams;
 
 function Home(): ReactElement {
   return (
