e8201d4ac714b1219a1a804e28f8cfc8cbb192c6
Fabian Friedrich <friedrich.fabian@gmx.net>
2023-03-29 20:50:02 +0200

    Fixed hydration error of homegrid

--- jamalyzer-client/app/home/HomeRecommended.tsx
+++ jamalyzer-client/app/home/HomeRecommended.tsx
@@ -5,17 +5,7 @@ import jamList from "@/public/jamList.json";
 import { jamCard } from "@/model/jamData/jamCard";
 import HomeJamCard from "@/app/home/HomeJamCard";
 
-function shuffle(array: jamCard[]): jamCard[] {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
-const jams = shuffle(jamList.jams);
-
-function HomeRecommended() {
+function HomeRecommended({ jams }: { jams: jamCard[] }) {
   const gridRef = useRef<HTMLDivElement>(null);
   const [columnCount, setColumnCount] = useState<number>(4);
 
