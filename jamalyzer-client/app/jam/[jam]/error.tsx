c63b8b5dc729199ae5edaedcb1632c2900bab341
Fabian Friedrich <68951860+Fabianofski@users.noreply.github.com>
2023-04-15 18:57:12 +0200

    Redesigned Error page with placeholder image

--- jamalyzer-client/app/jam/[jam]/error.tsx
+++ jamalyzer-client/app/jam/[jam]/error.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import styles from "@/styles/jam/error.module.css";
+import buttonStyles from "@/styles/home/HomeJamCard.module.css";
+import Image from "next/image";
 
 export default function Error({
   error,
@@ -12,9 +14,32 @@ export default function Error({
 }) {
   return (
     <div className={styles["error-container"]}>
-      <h1>OOPS!</h1>
-      <p className={styles.message}>{error.message}</p>
-      <a href={"/"}>Try a different Jam...</a>
+      <div className={styles.container}>
+        <Image
+          src={"/assets/error/jam-jar.jpg"}
+          alt={"shattered jam jar"}
+          width={400}
+          height={300}
+          style={{ objectFit: "contain" }}
+        />
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <p style={{ margin: "0" }}>Art by:&nbsp;</p>
+          <a
+            href={"https://www.santumerino.com"}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.credits}
+          >
+            santumerino
+          </a>
+        </div>
+      </div>
+      <div className={styles.container}>
+        <p className={styles.message}>{error.message}</p>
+        <a href={"/"} className={buttonStyles["analyze-button"]}>
+          Back
+        </a>
+      </div>
     </div>
   );
 }
